37aadd7a36a06bfb1a3f83c490fa087aceb136b1
punkushal <spycymagar@gmail.com>
2025-01-22 14:48:55 +0545

    custom app text widget added

---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+  final TextAlign textAlign;
+
+  const AppText({
+    super.key,
+    required this.text,
+    this.fontSize = 16.0,
+    this.fontWeight = FontWeight.normal,
+    this.color = Colors.black,
+    this.textAlign = TextAlign.start,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+      ),
+    );
+  }
+}
