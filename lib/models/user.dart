d19a5c67b5afa8d72bee9e0e038cfd2d1e5252b6
punkushal <spycymagar@gmail.com>
2025-01-23 12:09:21 +0545

    method name changed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,16 +6,16 @@ class UserModel {
   UserModel({required this.email, required this.uid, required this.name});
 
   //To convert from json to User object
-  factory UserModel.fromJson(Map<String, dynamic> json) {
+  factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
-      email: json['email'],
-      uid: json['uid'],
-      name: json['displayName'],
+      email: map['email'],
+      uid: map['uid'],
+      name: map['displayName'],
     );
   }
 
   //To convert from User object to json
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'uid': uid,
       'email': email,
