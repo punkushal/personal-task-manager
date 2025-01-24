5996e34bff6da3725975fbfd2f3b20739260346f
punkushal <spycymagar@gmail.com>
2025-01-24 18:13:18 +0545

    bottom navigation bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:personal_task_manager/screens/add_task_screen.dart';
+import 'package:personal_task_manager/services/auth_service.dart';
+import 'package:personal_task_manager/widgets/task_list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final AuthService authService = AuthService();
+  int _currentIndex = 0;
+  List<Widget> screens = [
+    TaskListScreen(),
+    Center(
+      child: Text('profile screen'),
+    ),
+  ];
+  void onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Personal Task Manager'),
-      ),
+      body: screens[_currentIndex],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (ctx) => AddTaskScreen(
-                userId: '1',
+                userId: authService.auth.currentUser!.uid,
               ),
             ),
           );
         },
         child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task),
+            label: 'Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: onTapped,
       ),
     );
   }
