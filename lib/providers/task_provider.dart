df821525edce0f9960bde33355ba2bc9a689150e
punkushal <spycymagar@gmail.com>
2025-01-25 16:44:13 +0545

    predefined categories added

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:personal_task_manager/models/category_model.dart';
 
 import '../models/task_model.dart';
 import '../services/task_service.dart';
@@ -7,6 +9,8 @@ class TaskProvider with ChangeNotifier {
   bool isVisible = false;
 
   final TaskService _taskService = TaskService();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<CategoryModel> _categories = [];
 
   Priority? _selectedPriority;
   Priority? _appliedPriority;
@@ -19,6 +23,18 @@ class TaskProvider with ChangeNotifier {
   DateTime? get startDate => _startDate;
   DateTime? get endDate => _endDate;
   bool get isFiltering => _isFiltering;
+
+  final List<CategoryModel> _predefinedCategories = [
+    CategoryModel(name: 'Food'),
+    CategoryModel(name: 'Education'),
+    CategoryModel(name: 'Travel'),
+    CategoryModel(name: 'Health'),
+    CategoryModel(name: 'Entertainment'),
+  ];
+  List<CategoryModel> get categories => [
+        ..._predefinedCategories,
+        ..._categories,
+      ];
 
   void setPriority(Priority? priority) {
     _selectedPriority = priority;
@@ -54,8 +70,45 @@ class TaskProvider with ChangeNotifier {
       userId,
       priority: _isFiltering ? _appliedPriority : null,
       startDate: _isFiltering ? _startDate : null,
-      endDate: _isFiltering ? _endDate : null,
     );
+  }
+
+  Future<void> addCategory(CategoryModel category, String userId) async {
+    try {
+      final docRef = await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('categories')
+          .add(category.toMap());
+
+      category = CategoryModel(
+        id: docRef.id,
+        name: category.name,
+      );
+
+      _categories.add(category);
+      notifyListeners();
+    } catch (e) {
+      print('Error adding category: $e');
+    }
+  }
+
+  Future<void> fetchCategories(String userId) async {
+    try {
+      final categoriesSnapshot = await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('categories')
+          .get();
+
+      _categories = categoriesSnapshot.docs
+          .map((doc) => CategoryModel.fromMap(doc.data(), doc.id))
+          .toList();
+
+      notifyListeners();
+    } catch (e) {
+      print('Error fetching categories: $e');
+    }
   }
 
   void toggleVissibility() {
