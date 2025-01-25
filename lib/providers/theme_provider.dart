3362a51a0021803fef8d513ffaa9385ac18ccaf7
punkushal <spycymagar@gmail.com>
2025-01-25 18:41:34 +0545

    theme provider added

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkMode = false;
+  static const String _themePreferenceKey = 'isDarkMode';
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeProvider() {
+    _loadThemeFromPrefs();
+  }
+
+  void _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_themePreferenceKey) ?? false;
+    notifyListeners();
+  }
+
+  void toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themePreferenceKey, _isDarkMode);
+    notifyListeners();
+  }
+
+  ThemeMode get themeMode => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
+}
