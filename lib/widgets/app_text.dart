a6be49dd9b60f3d74514cd1e3adb2c4c6f2d1a9f
punkushal <spycymagar@gmail.com>
2025-01-25 18:40:42 +0545

    theme provider added for dark mode

--- lib/widgets/app_text.dart
+++ lib/widgets/app_text.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:personal_task_manager/providers/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class AppText extends StatelessWidget {
   final String text;
@@ -18,14 +20,18 @@ class AppText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      textAlign: textAlign,
-      style: TextStyle(
-        fontSize: fontSize,
-        fontWeight: fontWeight,
-        color: color,
-      ),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return Text(
+          text,
+          textAlign: textAlign,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+            color: themeProvider.isDarkMode ? Colors.white : color,
+          ),
+        );
+      },
     );
   }
 }
