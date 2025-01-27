fc7fa0b077d89780fee09254959303aee7f10fcf
punkushal <spycymagar@gmail.com>
2025-01-27 10:27:07 +0545

    unneccesary code removed

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:personal_task_manager/models/category_model.dart';
 import 'package:personal_task_manager/services/auth_service.dart';
@@ -9,7 +8,6 @@ class TaskProvider with ChangeNotifier {
   bool isVisible = false;
 
   final TaskService _taskService = TaskService();
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final List<CategoryModel> _categories = [];
   List<TaskModel> _tasksList = [];
   List<TaskModel> get tasksList => _tasksList;
@@ -97,26 +95,6 @@ class TaskProvider with ChangeNotifier {
       startDate: _isFiltering ? _startDate : null,
       category: _isFiltering ? _appliedCategory : null,
     );
-  }
-
-  Future<void> addCategory(CategoryModel category, String userId) async {
-    try {
-      final docRef = await _firestore
-          .collection('users')
-          .doc(userId)
-          .collection('categories')
-          .add(category.toMap());
-
-      category = CategoryModel(
-        id: docRef.id,
-        name: category.name,
-      );
-
-      _categories.add(category);
-      notifyListeners();
-    } catch (e) {
-      print('Error adding category: $e');
-    }
   }
 
   void toggleVissibility() {
