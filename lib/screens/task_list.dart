04793dbd70f5ef6f4a977f19d90708b9c02e3fd0
punkushal <spycymagar@gmail.com>
2025-02-02 10:54:57 +0545

    filtering model sheet bottom modified

--- lib/screens/task_list.dart
+++ lib/screens/task_list.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:personal_task_manager/providers/task_provider.dart';
-import 'package:personal_task_manager/screens/add_task_screen.dart';
 import 'package:personal_task_manager/services/auth_service.dart';
-import 'package:personal_task_manager/utils/helper_function.dart';
 import 'package:personal_task_manager/widgets/app_text.dart';
-import 'package:personal_task_manager/widgets/task_detail.dart';
 import 'package:provider/provider.dart';
 import '../models/task_model.dart';
 import '../services/task_service.dart';
+import '../widgets/task_list_tile.dart';
 
 class TaskListScreen extends StatefulWidget {
   const TaskListScreen({super.key});
@@ -21,7 +19,6 @@ class _TaskListScreenState extends State<TaskListScreen> {
 
   // Filter variables
   DateTime? _startDate;
-  DateTime? _endDate;
 
   void _showFilterDialog() {
     showModalBottomSheet(
@@ -40,7 +37,7 @@ class _TaskListScreenState extends State<TaskListScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       DropdownButton<Priority>(
-                        hint: Text('Select Priority'),
+                        hint: AppText(text: 'Select Priority'),
                         value: taskProvider.selectedPriority,
                         onChanged: (Priority? newValue) {
                           taskProvider.setPriority(newValue);
@@ -55,7 +52,7 @@ class _TaskListScreenState extends State<TaskListScreen> {
                       ),
                       DropdownButton<String>(
                         value: taskProvider.selectedCategory,
-                        hint: Text('Category'),
+                        hint: AppText(text: 'Category'),
                         items: taskProvider.categories
                             .map((category) => DropdownMenuItem(
                                   value: category.name,
@@ -70,50 +67,29 @@ class _TaskListScreenState extends State<TaskListScreen> {
                   ),
 
                   // Date Range Picker
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            final pickedDate = await showDatePicker(
-                              context: ctx,
-                              initialDate: _startDate ?? DateTime.now(),
-                              firstDate: DateTime(2000),
-                              lastDate: DateTime(2101),
-                            );
-                            if (pickedDate != null) {
-                              taskProvider.setStartDate(pickedDate);
-                            }
-                          },
-                          child: Text(taskProvider.startDate == null
-                              ? 'Due Date'
-                              : taskProvider.startDate!
-                                  .toLocal()
-                                  .toString()
-                                  .split(' ')[0]),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            final pickedDate = await showDatePicker(
-                              context: ctx,
-                              initialDate: _endDate ?? DateTime.now(),
-                              firstDate: DateTime(2000),
-                              lastDate: DateTime(2101),
-                            );
-                            setState(() {
-                              _endDate = pickedDate;
-                            });
-                          },
-                          child: Text(_endDate == null
-                              ? 'End Date'
-                              : 'To: ${_endDate!.toLocal().toString().split(' ')[0]}'),
-                        ),
-                      ),
-                    ],
+                  ElevatedButton(
+                    onPressed: () async {
+                      final pickedDate = await showDatePicker(
+                        context: ctx,
+                        initialDate: _startDate ?? DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2101),
+                      );
+                      if (pickedDate != null) {
+                        taskProvider.setStartDate(pickedDate);
+                      }
+                    },
+                    child: AppText(
+                      text: taskProvider.startDate == null
+                          ? 'Due Date'
+                          : taskProvider.startDate!
+                              .toLocal()
+                              .toString()
+                              .split(' ')[0],
+                      color: Colors.white,
+                    ),
                   ),
+                  SizedBox(width: 10),
 
                   // Apply Filters Button
                   ElevatedButton(
@@ -121,7 +97,10 @@ class _TaskListScreenState extends State<TaskListScreen> {
                       taskProvider.applyFilters();
                       Navigator.pop(ctx);
                     },
-                    child: Text('Apply Filters'),
+                    child: AppText(
+                      text: 'Apply Filters',
+                      color: Colors.white,
+                    ),
                   ),
 
                   // Reset Filters Button
@@ -130,7 +109,7 @@ class _TaskListScreenState extends State<TaskListScreen> {
                       taskProvider.resetFilters();
                       Navigator.pop(ctx);
                     },
-                    child: Text('Reset Filters'),
+                    child: AppText(text: 'Reset Filters'),
                   ),
                 ],
               ),
@@ -197,127 +176,6 @@ class _TaskListScreenState extends State<TaskListScreen> {
                 },
               );
             },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class TaskListTile extends StatelessWidget {
-  final TaskModel task;
-  final String userId;
-  final TaskService taskService;
-
-  const TaskListTile({
-    super.key,
-    required this.task,
-    required this.userId,
-    required this.taskService,
-  });
-
-  Color _getPriorityColor() {
-    switch (task.priority) {
-      case Priority.high:
-        return Colors.red;
-      case Priority.medium:
-        return Colors.orange;
-      case Priority.low:
-        return Colors.green;
-    }
-  }
-
-  Future<void> _deleteTask(
-      BuildContext context, TaskService taskService, TaskModel task) async {
-    try {
-      await taskService.deleteTask(task.id!);
-    } catch (e) {
-      if (context.mounted) {
-        Navigator.pop(context); // Close dialog
-      }
-      if (context.mounted) {
-        showMsg(
-            context, 'Failed to delete the task :${e.toString()}', Colors.red);
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: _getPriorityColor(),
-          child: Icon(Icons.task, color: Colors.white),
-        ),
-        title: AppText(
-          text: task.title,
-          fontWeight: FontWeight.bold,
-        ),
-        subtitle: Text(
-          '${task.dueDate.toLocal().toString().split(' ')[0]} - ${task.description ?? ''}',
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        trailing: PopupMenuButton(
-          itemBuilder: (ctx) => [
-            PopupMenuItem(
-              value: 'delete',
-              child: Text('Delete'),
-            ),
-            PopupMenuItem(
-              value: 'edit',
-              child: Text('Edit'),
-            ),
-          ],
-          onSelected: (value) {
-            if (value == 'edit') {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (ctx) => AddTaskScreen(
-                    userId: userId,
-                    existingTask: task,
-                  ),
-                ),
-              );
-            } else if (value == 'delete') {
-              showDialog(
-                  context: context,
-                  builder: (ctx) {
-                    return AlertDialog(
-                      title: Text('Delete Task'),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          AppText(text: 'Are you sure to delete this task?'),
-                        ],
-                      ),
-                      actions: [
-                        ElevatedButton(
-                          onPressed: () {
-                            _deleteTask(context, taskService, task);
-                            Navigator.pop(context);
-                            showMsg(
-                              context,
-                              'Successfully deleted the task',
-                              Colors.blue,
-                            );
-                          },
-                          child: Center(child: Text('Yes')),
-                        ),
-                      ],
-                    );
-                  });
-            }
-          },
-        ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (ctx) => TaskDetail(task: task),
-            ),
           );
         },
       ),
